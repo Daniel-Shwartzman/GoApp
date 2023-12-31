d1d0ebd1f36bb8fd05462668c1285929ccbd5983
Daniel Shwartzman <dshwartzman5@gmail.com>
2023-12-31 10:39:42 +0200

    tests

---
+++ app_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"strings"
+)
+
+func TestHandleRequests(t *testing.T) {
+	// Create a new HTTP request (a mock request for testing)
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new ResponseRecorder (which implements http.ResponseWriter) to record the response
+	rr := httptest.NewRecorder()
+
+	// Create an HTTP handler from the handleRequests function
+	handler := http.HandlerFunc(handleRequests)
+
+	// Serve the HTTP request to the handler
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response Heading
+	expectedHeading := "<h1>Test</h1>"
+	if !strings.Contains(rr.Body.String(), expectedHeading) {
+		t.Errorf("Handler returned body without expected heading: got %v want %v", rr.Body.String(), expectedHeading)
+	}
+}
